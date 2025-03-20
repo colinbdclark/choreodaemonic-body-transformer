1c193f76d232b05940312bde29b3a09814c4b05e
Colin Clark <colinbdclark@gmail.com>
2025-03-20 16:22:10 -0400

    Resolves gh-25: Clamps robot joint coordinates to actuator safe zones.

--- src/renderer/robot-joints.js
+++ src/renderer/robot-joints.js
@@ -1,3 +1,86 @@
+const JOINT_LIMITS = [
+    {
+        x: {
+            min: -2.97,
+            max: 0.23
+        },
+        y: {
+            min: 0.0,
+            max: 3.05
+        }
+    },
+    {
+        x: {
+            min: -2.51,
+            max: 0.67
+        },
+        y: {
+            min: 0.0,
+            max: 3.05
+        }
+    },
+    {
+        x: {
+            min: -2.06,
+            max: 1.14
+        },
+        y: {
+            min: 0.0,
+            max: 3.05
+        }
+    },
+    {
+        x: {
+            min: -1.6,
+            max: 1.6
+        },
+        y: {
+            min: 0.0,
+            max: 3.05
+        }
+    },
+    {
+        x: {
+            min: -1.14,
+            max: 2.06
+        },
+        y: {
+            min: 0.0,
+            max: 3.05
+        }
+    },
+    {
+        x: {
+            min: -0.67,
+            max: 2.51
+        },
+        y: {
+            min: 0.0,
+            max: 3.05
+        }
+    },
+    {
+        x: {
+            min: -0.23,
+            max: 2.97
+        },
+        y: {
+            min: 0.0,
+            max: 3.05
+        }
+    }
+];
+
+function limitJoint(jointAxisValue, jointLimit) {
+    if (jointAxisValue < jointLimit.min) {
+        jointAxisValue = jointLimit.max;
+    } else if (jointAxisValue > jointLimit.max) {
+        jointAxisValue = jointLimit.max;
+    }
+
+    return jointAxisValue;
+}
+
 export class RobotJoints {
     constructor(homePositions, scaling, mappings) {
         /*
@@ -30,18 +113,30 @@ export class RobotJoints {
         }
     }
 
+    bodyToJointX(keypointX, jointLimit) {
+        let jointX = (keypointX + this.scaling.offsetX) *
+            this.scaling.scaleX;
+
+        return limitJoint(jointX, jointLimit);
+    }
+
+    bodyToJointY(keypointY, jointLimit) {
+        let jointY = (this.scaling.offsetY - keypointY) *
+            this.scaling.scaleY;
+
+        return limitJoint(jointY, jointLimit);
+    }
+
     update(keypointsModel) {
         for (let i = 0; i < this.mappings.length; i++) {
             let keypointName = this.mappings[i];
-
             let keypoint = keypointsModel[keypointName];
 
             if (keypoint) {
-                // Scale from frame coordinates to robot stage coordinates.
-                let robotX = (keypoint.x + this.scaling.offsetX) *
-                    this.scaling.scaleX;
-                let robotY = (this.scaling.offsetY - keypoint.y) *
-                    this.scaling.scaleY;
+                // Scale from frame coordinates to robot stage coordinates,
+                // and apply joint limits.
+                let robotX = this.bodyToJointX(keypoint.x, JOINT_LIMITS[i].x);
+                let robotY = this.bodyToJointY(keypoint.y, JOINT_LIMITS[i].y);
                 this.model[i] = [robotX, robotY];
             } else {
                 let homePosition = this.homePositions[i];
